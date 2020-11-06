8f1ba8a93fea99c5d11f319a7d567a3413987494
CAUGANT Guillaume <caugant@et.esiea.fr>
2020-11-06 21:41:45 +0100

    Cross origin spring boot, modification component helloworld

--- Back/src/main/java/com/esiea/backend/HomeController.java
+++ Back/src/main/java/com/esiea/backend/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:8080")
 public class HomeController {
     @Autowired
     UserService userService;
@@ -24,14 +25,14 @@ public class HomeController {
         return userService.getUser();
     }
 
-    @GetMapping("/user/{username}")
+    @GetMapping("/user/{username}") //user/root?username=toto
     @ResponseBody
     public User getUserByUsername(@RequestParam String username)
     {
         return userService.getUserByUsername(username);
     }
 
-    @PostMapping("/user/{username}{password}{name}")
+    @PostMapping("/user/{username}{password}{name}") //user/root?username=toto?password=root?name=Guillaume
     @ResponseBody
     public boolean createUser(@RequestParam String username, String password, String name)
     {
