e351336b1100d042c9dd3cac68b069f8dff65766
CAUGANT Guillaume <caugant@et.esiea.fr>
2020-11-07 17:47:11 +0100

    Generation du token

--- Back/src/main/java/com/esiea/backend/SecurityConfigurer.java
+++ Back/src/main/java/com/esiea/backend/SecurityConfigurer.java
@@ -4,7 +4,9 @@ import com.esiea.backend.services.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -18,8 +20,18 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     MyUserDetailsService myUserDetailsService;
 
     @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+    @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(myUserDetailsService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests().antMatchers("/authentification").permitAll().anyRequest().authenticated();
     }
 
     @Bean
