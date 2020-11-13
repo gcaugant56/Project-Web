a6dfacd3481ce38f06862dc5f3c068c4ff0c1855
CAUGANT Guillaume <caugant@et.esiea.fr>
2020-11-13 22:57:37 +0100

    Traitement du token depuis le header

--- Back/src/main/java/com/esiea/backend/controller/UserController.java
+++ Back/src/main/java/com/esiea/backend/controller/UserController.java
@@ -15,6 +15,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
 @CrossOrigin(origins = "http://localhost:5500")
 public class UserController {
@@ -50,11 +52,11 @@ public class UserController {
 
     @GetMapping("/user")
     @ResponseBody
-    public User getUserByUsername(@RequestBody String token)
+    public User getUserByUsername(@RequestHeader("Authorization") Map<String, String> headers)
     {
-        String[] jsonParse;
-        jsonParse = token.split("\"");
-        String username = Tokenutil.extractUsername(jsonParse[3]);
+        String token = headers.get("authorization");
+        token = token.substring(7);
+        String username = Tokenutil.extractUsername(token);
         User user =  userService.getUserByUsername(username);
         user.hiddenPassword();
         return user;
