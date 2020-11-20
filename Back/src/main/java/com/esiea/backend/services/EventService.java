cb5fd6bb4d53a77a31bb87e0ce02b87ac9218d81
CAUGANT Guillaume <caugant@et.esiea.fr>
2020-11-20 17:43:49 +0100

    Récupération des événements pour un utilisateur donné

--- Back/src/main/java/com/esiea/backend/services/EventService.java
+++ Back/src/main/java/com/esiea/backend/services/EventService.java
@@ -2,13 +2,19 @@ package com.esiea.backend.services;
 
 import com.esiea.backend.Event;
 import com.esiea.backend.repository.EventRepository;
+import com.esiea.backend.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EventService {
     @Autowired
     private EventRepository eventRepository;
+
+    @Autowired
+    private JwtUtil TokenUtil;
 
     public Event getEventByName(String name)
     {
@@ -26,4 +32,14 @@ public class EventService {
         return true;
     }
 
+    public List<Event> getEventForUser(String username, String usernames)
+    {
+        return eventRepository.findAllByParticipantContainingOrCreatorEquals(username, usernames);
+    }
+
+    public String getUsernameFromToken(String token)
+    {
+        token = token.substring(7);
+        return TokenUtil.extractUsername(token);
+    }
 }
