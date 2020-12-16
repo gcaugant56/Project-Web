3cd718da4ea80bf4ba04edb6e0f7619b1dbf34ca
Valentin camozzi <camozzi@et.esiea.fr>
2020-12-16 01:00:10 +0100

    oupsi j'avais oublié de save

--- Front/src/assets/js/displayMenu.js
+++ Front/src/assets/js/displayMenu.js
@@ -5,6 +5,7 @@ let connexion = document.getElementById("connexion");
 console.log($cookies.get("token"));
 console.log("script JS TEST");
 
+
 if ($cookies.get("token")){
     inscription.style.display = "none";
     connexion.style.display = "none";
