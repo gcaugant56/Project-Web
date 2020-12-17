8846bb39dd5c377bef15db11cc9e5661e60b7091
Guillaume Caugant <caugant@et.esiea.fr>
2020-12-17 15:06:50 +0100

    Recuperation de toute les valeurs

--- Front/src/components/Planif.vue
+++ Front/src/components/Planif.vue
@@ -4,7 +4,14 @@
     <p>Nom d'évènements : </p>
     <input type='text' id='eventName'>  
     <p> Date : </p>
-    <input type='text' id='date'>  
+    <div class="form-group" v-for="(input,j) in inputsDate" :key="generateKey(input,j)">
+    <input type="text" class="form-control" v-model="input.name">
+    <span>
+        <i class="fas fa-minus-circle" @click="removeDate(j)" v-show="j || ( !j && inputsDate.length > 1)"></i>
+        <i class="fas fa-plus-circle" @click="addDate(j)" v-show="j == inputsDate.length-1"></i>
+    </span>
+    </div>
+
     <p>lieux : </p>
     <input type='text' id='place'>  
     
@@ -13,7 +20,7 @@
     <input type="text" class="form-control" v-model="input.name">
     <span>
         <i class="fas fa-minus-circle" @click="removeParticipant(k)" v-show="k || ( !k && inputsParticipant.length > 1)"></i>
-        <i class="fas fa-plus-circle" @click="addPartcipant(k)" v-show="k == inputsParticipant.length-1"></i>
+        <i class="fas fa-plus-circle" @click="addParticipant(k)" v-show="k == inputsParticipant.length-1"></i>
     </span>
     </div>
     <br>
@@ -28,20 +35,39 @@ export default {
     data: ()=>{
         return {
             inputsParticipant: [ {name: ''}],
+            inputsDate: [ {name: ''}],
+
         }
     },
     methods: {
-        addPartcipant(index) {
+        addParticipant(index) {
             this.inputsParticipant.push({ name: '' });
         },
         removeParticipant(index) {
             this.inputsParticipant.splice(index, 1);
         },
+        addDate(index) {
+            this.inputsDate.push({ name: '' });
+        },
+        removeDate(index) {
+            this.inputsDate.splice(index, 1);
+        },
+        generateKey(item,index){
+            const uniqueKey = `${item}-${index}`;
+            return uniqueKey;
+        },
         createEvent(){
             var participants = '';
+            var dates = '';
+            var place = document.getElementById("place").value;
+            var name = document.getElementById("eventName").value;
             this.inputsParticipant.forEach(participant => participants = participants + participant.name+',');
             participants = participants.slice(0,-1);
-        }
+            this.inputsDate.forEach(date => dates = dates + date.name+',');
+            dates = dates.slice(0,-1);
+            
+            
+        },
     }
 }
 </script>
